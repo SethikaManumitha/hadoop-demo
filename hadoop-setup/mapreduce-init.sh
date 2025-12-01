3d7d3bc25ad31af5837056b3e087db6c6bf65bd9
Sethika Manumitha <sethika7@gmail.com>
2025-12-01 13:47:47 +0530

    Fix map reduce sh

--- hadoop-setup/mapreduce-init.sh
+++ hadoop-setup/mapreduce-init.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-sudo docker exec -it namenode bash
-mkdir -p /opt/hadoop/resources
-mkdir -p /user/data/location/
-mkdir -p /user/data/weather/
-exit
-sudo docker cp /home/iitgcpuser/hadoop-demo/data-sources/processed_location_data.csv namenode:/user/data/location/
-sudo docker cp /home/iitgcpuser/hadoop-demo/data-sources/processed_weather_data.csv namenode:/user/data/weather
+
+# Create dirs inside namenode container
+sudo docker exec namenode mkdir -p /opt/hadoop/resources
+sudo docker exec namenode mkdir -p /user/data/location/
+sudo docker exec namenode mkdir -p /user/data/weather/
+
+# Copy files to container paths
+sudo docker cp /home/iitgcpuser/hadoop-demo/data-sources/processed_location_data.csv \
+    namenode:/user/data/location/
+
+sudo docker cp /home/iitgcpuser/hadoop-demo/data-sources/processed_weather_data.csv \
+    namenode:/user/data/weather/
+
+# Fix permissions for your scripts
 cd /home/iitgcpuser/hadoop-demo/MapReduce
-sudo chmod +X DistrictPerMonth.sh
-sudo chmod +X TopPrecipitation.sh
+sudo chmod +x DistrictPerMonth.sh
+sudo chmod +x TopPrecipitation.sh
